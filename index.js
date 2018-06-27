f193271ef6cc4972bb018565c42abfc3321a6af0
Taylor Morris <taylor.morris@gmail.com>
2018-06-27 13:40:51 -0700

    more XPATH work, getting promise errors on removeInvitee

--- index.js
+++ index.js
@@ -7,10 +7,38 @@ const driver = new selenium.Builder()
 
 driver.get(process.env.URL);
 
+const invitees = [
+   'Gonzalo Torres del Fierro',
+   'Shadd Anderson',
+   'George Aparece',
+   'Shadab Khan',
+   'Joseph Michael Casey',
+   'Jennifer Nordell',
+   'Faisal Albinali',
+   'Taron Foxworth',
+   'David Riesz',
+   'Maicej Torbus',
+   'Martin Luckett',
+   'Joel Bardsley',
+   'Reuben Varzea',
+   'Ken Alger',
+   'Amrit Pandey',
+   'Rafal Rudzinski',
+   'Brian Lynch',
+   'Lupe Camacho',
+   'Luke Fiji',
+   'Sean Christensen',
+   'Philip Graf',
+   'Mike Norman',
+   'Michael Hulet',
+   'Brent Suggs'
+];
+
 const locators = {
   inviteeForm: By.id("registrar"),
   inviteeNameField: By.css("#registrar input[name='name']"),
-  toggleNonReponsdersVisibility: By.css(".main > div input")
+  toggleNonReponsdersVisibility: By.css(".main > div input"),
+  removeButtonForInvitee: invitee => By.xpath(`//span[text()="${invitee}"]/../button[2]`)
 };
 
 function addInvitee(name) {
@@ -24,7 +52,11 @@ function toggleNonReponsdersVisibility() {
     .click();
 }
 
-addInvitee("Muhamed Hassan");
-addInvitee("Steve Harvey");
+function removeInvitee(invitee) {
+  driver.findElement(locators.removeButtonForInvitee(invitee))
+    .click();
+}
 
-toggleNonReponsdersVisibility();
+invitees.forEach(addInvitee);
+// removeInvitee("Shadd Anderson");
+// getting weird promise error here, others experiencing it too
